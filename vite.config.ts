a2ce08a497ec6c324d314f046fc7558b15b9a224
Ahmed Habeila <sm3rta@yahoo.com>
2022-12-28 23:41:33 +0200

    code splitting

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,25 @@
 import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
 import mkcert from 'vite-plugin-mkcert';
+import solidPlugin from 'vite-plugin-solid';
+// @ts-ignore
+import packageJson from './package.json';
+const { dependencies } = packageJson;
+
+// these packages have to be bundled in the vendor bundle
+// they're needed while the app is booting
+const vendorDepsArray: string[] = [];
+
+function renderChunks(deps: Record<string, string>) {
+	let chunks: Record<string, Array<string>> = {};
+	Object.keys(deps).forEach((key) => {
+		// @ts-ignore
+		if (vendorDepsArray.includes(key) || key.startsWith('@types/')) return;
+		chunks[key] = [key];
+	});
+	return chunks;
+}
+
+console.log(renderChunks(dependencies));
 
 export default defineConfig({
 	plugins: [solidPlugin(), mkcert()],
@@ -10,5 +29,13 @@ export default defineConfig({
 		outDir: 'build',
 		// change v to true to use source-map-analyzer
 		sourcemap: false,
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					vendor: vendorDepsArray,
+					...renderChunks(dependencies),
+				},
+			},
+		},
 	},
 });
